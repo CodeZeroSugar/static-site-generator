61c374275a605582368e00048211a3f8f96576e3
CodeZeroSugar <icvelder@gmail.com>
2025-09-24 00:15:58 -0500

    added LeafNode child of HTMLNode, issue with using HTMLNode method in LeafNode method

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -10,7 +10,7 @@ class HTMLNode:
 
     def props_to_html(self):
         if self.props is None:
-            return None
+            return ""
 
         if not isinstance(self.props, dict):
             raise Exception("invalid input for HTMLNode props")
@@ -23,4 +23,21 @@ class HTMLNode:
         return html_attr
 
     def __repr__(self):
-        return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
+        return f"HTMLNode({self.tag}, {self.value}, children: {self.children}, {self.props})"
+
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, props)
+
+    def to_html(self):
+        if self.value is None:
+            raise ValueError
+        if self.tag is None:
+            return self.value
+        if self.props is None:
+            self.props = ""
+        else:
+            self.props = self.props_to_html()
+
+        return f"<{self.tag}{self.props}>{self.value}</{self.tag}>"
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -10,9 +10,45 @@ class TestHTMLNode(unittest.TestCase):
             "target": "_blank",
         }
         node = HTMLNode("a", "This is a link for google.com", None, test_prop)
+        self.assertEqual(
+            node.props_to_html(),
+            ' href="https://www.google.com" target="_blank"',
+        )
 
-        return node.props_to_html()
+    def test_values(self):
+        node = HTMLNode(
+            "div",
+            "I wish I could read",
+        )
+        self.assertEqual(
+            node.tag,
+            "div",
+        )
 
+        self.assertEqual(
+            node.value,
+            "I wish I could read",
+        )
 
-if __name__ == "__main__":
-    unittest.main()
+        self.assertEqual(
+            node.children,
+            None,
+        )
+
+        self.assertEqual(
+            node.props,
+            None,
+        )
+
+    def test_repr(self):
+        node = HTMLNode(
+            "p",
+            "What a strange world",
+            None,
+            {"class": "primary"},
+        )
+
+        self.assertEqual(
+            node.__repr__(),
+            "HTMLNode(p, What a strange world, children: None, {'class': 'primary'})",
+        )
